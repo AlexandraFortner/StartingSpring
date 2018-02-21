a1d855680df676378e8d0c8c344d53abaeb9aed5
Alexandra <afortner@basecampcodingacademy.org>
2018-02-21 10:45:55 -0600

    Finished lastThree function in ArraysAndLoopsController.java.
    Need to eventually make lastThree use @PostMapping and  @ RequestBody to replace @GetMapping and @RequestParam.

--- src/main/java/com/example/hello2/BasicsController.java
+++ src/main/java/com/example/hello2/BasicsController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class BasicsController {
 
     //Starting with duplicate of Starting-JS but with Java and SpringSecurity
