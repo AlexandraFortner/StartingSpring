be31fece9658d7d3681c31a66c131e2ab6536c30
Alexandra <afortner@basecampcodingacademy.org>
2018-02-23 10:18:49 -0600

    Finished differenceFromMinimum and sumOfLonger function using GetMapping.

--- src/main/java/com/example/hello2/ArraysAndLoopsController.java
+++ src/main/java/com/example/hello2/ArraysAndLoopsController.java
@@ -28,4 +28,40 @@ public class ArraysAndLoopsController {
         for (int i : l) total += i;
             return total;
         }
+
+    // Returns the sum of the longer Array.
+    // If both Arrays are the same length, returns the sum of both Arrays.
+    @GetMapping("/sumOfLonger")
+    public Integer sumOfLonger(int[] l1, int[] l2) {
+        if (l1.length > l2.length){
+            return sum(l1);
+        } else if (l1.length < l2.length){
+            return sum(l2);
+        } else if (l1.length == l2.length){
+            return sum(l1) + sum(l2);
+        } else {
+            return null;
+        }
     }
+
+
+    // Array[Float] -> Array[Float]
+    // Returns the difference between each cost and the minimum cost.
+    //
+    // For example, if our options were [1, 3, 3, 2, 5].
+    // The minimum cost would be 1, and the differences from that
+    // minimum would be [0, 2, 2, 1, 4]
+    @GetMapping("/differenceFromMiniumum")
+    public ArrayList<Double> Min (@RequestParam String x) {
+        String[] List = x.split("\\s");
+        ArrayList<Double> num_list = new ArrayList<>();
+        for(String num : List) {
+            num_list.add(Double.parseDouble(num));
+        }
+        double minimum = Collections.min(num_list);
+        ArrayList<Double> new_list = new ArrayList<>();
+        for (double numbers: num_list) {
+            new_list.add(numbers - minimum);
+        } return  new_list;
+    }
+}
